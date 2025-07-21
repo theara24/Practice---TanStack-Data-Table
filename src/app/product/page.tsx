2ff8640f12003b1cc2ff87cf11615314c1cc9d53
Chim Theara <chimtheara93@gmail.com>
2025-07-21 19:47:49 +0700

    complate carts

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,37 +5,40 @@ import Link from 'next/link';
 import { useGetProductsQuery } from '@/lib/api/productsApi';
 import { ProductType } from '@/types/productType';
 import Loading from '../loading';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '@/lib/features/cartSlice';
 
 export default function ProductPage() {
-  // Using RTK Query to fetch products
   const { data, isLoading, error } = useGetProductsQuery();
-  if (isLoading) return <Loading />;
-  if (error)
-    return <p className="text-red-500 text-center">Failed to load products</p>;
+  const dispatch = useDispatch();
+  if (isLoading) return <Loading/>;
+  if (error) return <p className='text-red-500 text-center'>Failed to load products</p>;
   const products = data?.products as ProductType[];
 
   return (
-    <section className="w-[90%] mx-auto my-10">
-      <h2 className="font-bold text-[24px] text-blue-500 mt-32 uppercase">
-        Product Page
-      </h2>
-
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+    <section className='w-[90%] mx-auto my-10'>
+      <h2 className='font-bold text-[24px] text-blue-500 mt-32 uppercase'>Product Page</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4'>
         {products.map((product: ProductType) => (
-          <Link
-            key={product.id}
-            href={`/product/${product.id}`}
-            className="no-underline"
-          >
-            <ProductCard
-              id={product.id}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-              thumbnail={product.thumbnail}
-              category={product.category}
-            />
-          </Link>
+          <div key={product.id} className='no-underline'>
+            <Link href={`/product/${product.id}`}>
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                thumbnail={product.thumbnail}
+                category={product.category}
+                hideAddToCart
+              />
+            </Link>
+            <button
+              onClick={() => dispatch(addToCart(product))}
+              className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            >
+              Add To Cart
+            </button>
+          </div>
         ))}
       </div>
     </section>
